a6c3c06af412d9dfa490af6f405200fcce807903
GEISEL APARECIDO DE MELO <93731138+GeiselMelo@users.noreply.github.com>
2023-02-11 19:30:53 -0300

    Add files via upload
    App calculadora

---
+++ app.js
@@ -0,0 +1,17 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.id === "=") {
+            display.value = eval(display.value);
+        } else if (btn.id === "ce") {
+            display.value = "";
+        } else if (btn.id == "c") {
+            display.value = display.value.slice(0, -1);
+        } else {
+            display.value += btn.id;
+        }
+    });
+});
+
